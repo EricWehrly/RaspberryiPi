f86aca602a210a9cfd090ad1b00b653366cf4516
EricWehrly <eric@disruptiontheory.com>
2020-04-22 23:48:03 -0400

    run docker as root, not pi

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# TODO: Propert start / stop
+# TODO: Proper start / stop
 # https://www.stuffaboutcode.com/2012/06/raspberry-pi-run-program-at-start-up.html
 
 cd /home/pi/docker
 rm -f docker-compose.log
-sudo -u pi docker-compose up > docker-compose.log
+docker-compose up > docker-compose.log
