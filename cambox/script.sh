69d45b04c229b9940c638623846d3006efb06a6a
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-05-14 20:08:19 -0400

    Update script.sh

--- cambox/script.sh
+++ cambox/script.sh
@@ -1,3 +1,9 @@
 # So far I just copied basically what raspy is doing 
 # but we need to "restore" our docker compose to here
 # https://github.com/plexinc/pms-docker
+
+# add plex's data folder and let valid samba users write to it
+net usershare add --long share /media/eric/barrac/plex/data "" "Everyone:F" guest_ok=n
+
+# maybe we need sonarr?
+# https://sonarr.tv/#downloads-docker
